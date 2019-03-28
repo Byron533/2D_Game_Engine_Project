5b2e7a4248fc84e6d81cc4f440e256824f8041bf
Byron533 <31711000+Byron533@users.noreply.github.com>
2019-03-28 00:00:14 -0400

    The rough outline/ last worked on: projectiles

---
+++ B_Engine - Textures/B_Engine/Src/ECS/Components.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "ECS.h"
+#include "TransformComponent.h"
+#include "SpriteComponent.h"
+#include "KeyboardController.h"
+#include "ColliderComponent.h"
+#include "TileComponent.h"
+#include "ProjectileComponent.h"
+
+
+//will be responsible for sprite and render components for player, enemy, tiles
